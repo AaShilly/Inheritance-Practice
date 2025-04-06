8fb24df633b25d57faf567abc9407d43dd789e40
anthony.shilly@gmail.com <anthonyshilly@gmail.com>
2025-04-06 14:34:07 -0400

    Added comments to teacher module

--- classes/teacher.js
+++ classes/teacher.js
@@ -2,12 +2,13 @@ const Person = require('./person');
 
 class Teacher extends Person {
   constructor(firstName, lastName, subject, yearsOfExperience){
-    super(firstName, lastName);
-    this.subject = subject;
-    this.yearsOfExperience = yearsOfExperience;
+    super(firstName, lastName); //Calls the parent class Person's constructor and passes first and last name parameters
+    this.subject = subject; //creates a subject property
+    this.yearsOfExperience = yearsOfExperience; //creates a years of expereince property
   }
 
   static combinedYearsOfExperience(teachers){
+    //Calculates the total years of combined experience between all teachers
     return teachers.reduce((accumulator, currentTeacher) => accumulator + currentTeacher.yearsOfExperience , 0)
   }
 }
