7080927be8cc0974b39ec5d396b5a8ec18b1a40b
anthony.shilly@gmail.com <anthonyshilly@gmail.com>
2025-04-06 14:31:13 -0400

    Completed assignment, pushing to repo

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName); // calls Person constructor utilizing first and last name properties from parent class Person
+    this.major = major; //major property for student class
+    this.GPA = GPA; //GPA property for student class
+  }
+
+  static compareGPA(student1, student2){
+    if (student1.GPA === student2.GPA){
+      return "Both students have the same GPA" //Checks if student has same GPA.
+    }
+
+    const higherGPA = student1.GPA > student2.GPA ? student1 : student2; //Ternary Operator to compare and select student with Higher GPA
+    return `${higherGPA.firstName} ${higherGPA.lastName} has the higher GPA.` //Shows which student has higher gpa.
+
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
